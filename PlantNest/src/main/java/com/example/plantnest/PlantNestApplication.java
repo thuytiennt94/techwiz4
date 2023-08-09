1693c2f60b8a3a049a8c9bbf29af3a8b6608647c
thuytiennt94 <thuytiennt449@gmail.com>
2023-08-09 14:17:44 +0700

    commit

---
+++ PlantNest/src/main/java/com/example/plantnest/PlantNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantNestApplication.class, args);
+    }
+
+}
---
+++ PlantNest/src/test/java/com/example/plantnest/PlantNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plantnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
